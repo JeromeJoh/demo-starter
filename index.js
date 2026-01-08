dd3ac7025da9bae62ac3bcc848697f2954c4c53e
JeromeJoh <jerome.yzhao@foxmail.com>
2026-01-08 09:33:06 +0800

    fix: trivia

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { LitElement, html, css } from 'https://unpkg.com/lit@latest?module';
 
 class MyApp extends LitElement {
+  constructor() {
+    super();
+    console.log('MyApp initialized');
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    console.log('MyApp connected to the DOM', this.shadowRoot);
+  }
+
   static styles = css`
     h1 {
       color: red;
